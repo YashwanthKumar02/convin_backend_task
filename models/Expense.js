9581051984aebc84ae81f438e680667a471b560b
YashwanthKumar02 <yashwanthnani2@gmail.com>
2024-07-28 19:59:42 +0530

    feat: create Expense model

---
+++ models/Expense.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const expenseSchema = new mongoose.Schema({
+    description: {
+        type: String,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    splitType: {
+        type: String,
+        required: true,
+        enum: ['equal', 'exact', 'percentage']
+    },
+    participants: [
+        {
+            user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+            amount: { type: Number }
+        }
+    ]
+});
+
+module.exports = mongoose.model('Expense', expenseSchema);
